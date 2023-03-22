f03f207db8858a574b274bef9054792283ec6264
OmerGit <rafaelxdale@gmail.com>
2023-03-22 02:40:59 -0500

    update

--- src/components/Home/Offer.js
+++ src/components/Home/Offer.js
@@ -12,22 +12,22 @@ const Offer = () => {
             </div>
             <div className="offer-box">
                 <Offerbox
-                    icon={<img className={"img-fluid"} src={fastFood} alt={"fastFood"}/>}
+                    icon={<img className={"mb-5"} src={fastFood} alt={"fastFood"}/>}
                     Ongoing_Offers="Ofertas en curso"
                     Box_description="Reserva online tus billetes de autobús y obtén increíbles descuentos"
                 />
                 <Offerbox
-                    icon={<img className={"img-fluid"} src={saveFood} alt={"fastFood"}/>}
+                    icon={<img className={"mb-5"} src={saveFood} alt={"fastFood"}/>}
                     Ongoing_Offers=" Reserva de comida"
                     Box_description="Ofrecemos Servicio de Alimentación al Embarque. Bebidas, biryani, etc."
                 />
                 <Offerbox
-                    icon={<img className={"img-fluid"} src={maps} alt={"fastFood"}/>}
+                    icon={<img className={"mb-5"} src={maps} alt={"fastFood"}/>}
                     Ongoing_Offers="Localización de autobuses"
                     Box_description="Ofrecemos las mejores herramientas para localizar nuestro autobús en curso"
                 />
                 <Offerbox
-                    icon={<img className={"img-fluid"} src={money} alt={"fastFood"}/>}
+                    icon={<img className={"mb-5"} src={money} alt={"fastFood"}/>}
                     Ongoing_Offers="Ahorra y gana"
                     Box_description="Tenemos un fuerte marketing de referencia. Comparte código promocional y gana dinero"
                 />
